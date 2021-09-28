5ec59274a4fd0cb5156d713538a2ab51996fb2b2
yizhou zhou <yizhouzhou@yizhous-MacBook-Pro.local>
2021-09-28 16:51:03 -0400

    complete MyButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import "./styles/index.scss";
-import MyButton from "./components/MyButton/MyButton"
+import MyButton, {
+  ButtonSize,
+  ButtonType,
+} from "./components/MyButton/MyButton";
 
-import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="App">
-      <MyButton/>
+      <h1>Adam UI</h1>
+      <h4>Default</h4>
+      <MyButton>Default</MyButton>
+      <MyButton disabled>Disabled</MyButton>
+      <MyButton isDark>Dark Button</MyButton>
+      <MyButton className="btn-red">Additional Class Name</MyButton>
+      <h4>Sizes</h4>
+      <MyButton buttonSize={ButtonSize.Small}>Small Button</MyButton>
+      <MyButton buttonSize={ButtonSize.Large}>Large Button</MyButton>
+      <h4>Types</h4>
+      <MyButton >Default Button</MyButton>
+      <MyButton buttonType={ButtonType.Primary}>Primary Button</MyButton>
+      <MyButton buttonType={ButtonType.Secondary}>Secondary Button</MyButton>
+      <MyButton buttonType={ButtonType.Danger}>Danger Button</MyButton>
+      <MyButton href={"https://www.google.com"}  buttonType={ButtonType.Link}>Link Button</MyButton>
+      <MyButton  href={"https://www.google.com"} buttonType={ButtonType.Link} disabled>Link Button</MyButton>
     </div>
   );
 }
