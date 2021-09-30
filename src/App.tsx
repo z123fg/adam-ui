0a0ae4aa2ec7227b5a60840706368f8a617f2465
Chingche Lin <61772538+cclap2020@users.noreply.github.com>
2021-09-30 16:27:38 -0400

    This is a heavy commit, including css style, structure of the component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import MyButton, {
   ButtonSize,
   ButtonType,
 } from "./components/MyButton/MyButton";
+import AnimatedHoverCard, {
+  CardType,
+  CardStyle,
+} from "./components/AnimatedHoverCard/AnimatedHoverCard";
+import img from "./components/AnimatedHoverCard/img/img1.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>Adam UI</h1>
+      {/* <h1>Adam UI</h1>
       <h4>Default</h4>
       <MyButton>Default</MyButton>
       <MyButton disabled>Disabled</MyButton>
@@ -23,7 +28,13 @@ function App() {
       <MyButton buttonType={ButtonType.Secondary}>Secondary Button</MyButton>
       <MyButton buttonType={ButtonType.Danger}>Danger Button</MyButton>
       <MyButton href={"https://www.google.com"}  buttonType={ButtonType.Link}>Link Button</MyButton>
-      <MyButton  href={"https://www.google.com"} buttonType={ButtonType.Link} disabled>Link Button</MyButton>
+      <MyButton  href={"https://www.google.com"} buttonType={ButtonType.Link} disabled>Link Button</MyButton> */}
+      <AnimatedHoverCard
+        className="Test"
+        imgSrc={img}
+        cardStyle={CardStyle.Style1}
+        cardType={CardType.Text}
+      />
     </div>
   );
 }
--- src/components/AnimatedHoverCard/AnimatedHoverCard.tsx
+++ src/components/AnimatedHoverCard/AnimatedHoverCard.tsx
@@ -1,1 +1,59 @@
-import React from "react";
+import React, { FC, ReactNode } from "react";
+import "./AnimatedHoverCard.css";
+import img1 from "./img/img1.png";
+import genClassName from "../../util/genClassName";
+
+export enum CardType {
+  Pure = "pure",
+  Text = "text",
+}
+
+export enum CardStyle {
+  Style1 = "card-style-1",
+  Style2 = "card-style-2",
+}
+
+interface IAnimatedCardProps {
+  cardType?: CardType;
+  className?: string;
+  description?: string;
+  imgSrc?: string;
+  cardStyle: string;
+}
+
+const AnimatedHoverCard: FC<IAnimatedCardProps> = ({
+  cardType = CardType.Text,
+  className = "",
+  description = "",
+  imgSrc = img1,
+  cardStyle = CardStyle.Style1,
+}) => {
+  let classNameStr = genClassName(cardStyle, className).join(" ");
+  console.log(classNameStr);
+
+  if (cardType === CardType.Text) {
+    return (
+      //for text
+      <div className="card-container">
+        <section className="card-text">
+          <h3>Test</h3>
+          <p>Paragraph</p>
+        </section>
+        <div className={classNameStr}>
+          <img src={imgSrc} alt="img" />
+        </div>
+      </div>
+    );
+  } else {
+    //for pure
+    return (
+      <div className="card-container">
+        <div className="card-style-1">
+          <img src={imgSrc} alt="img" />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default AnimatedHoverCard;
