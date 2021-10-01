1940c11b18824d333b60ebbec865073d4afa244b
Yuhui Shen <yuhui.shen@hotmail.com>
2021-10-01 17:43:56 -0400

    remove change to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import MyButton, {
 	ButtonSize,
 	ButtonType,
 } from "./components/MyButton/MyButton";
-import MyIcon, { IconSize, IconColor } from "./components/MyIcon/MyIcon";
 
 function App() {
 	return (
@@ -36,39 +35,6 @@ function App() {
 				disabled>
 				Link Button
 			</MyButton>
-			{/* <div>
-				<MyIcon name="home" />
-				<MyIcon name="users" />
-				<MyIcon name="search" />
-				<MyIcon name="star" />
-				<MyIcon name="moon" />
-				<MyIcon name="credit card" />
-				<MyIcon name="info" />
-				<MyIcon name="check" />
-				<MyIcon name="heart" />
-			</div>
-			<div>
-				<MyIcon name="home" size={IconSize.Large} />
-				<MyIcon name="users" size={IconSize.Large} />
-				<MyIcon name="search" size={IconSize.Large} />
-				<MyIcon name="star" size={IconSize.Large} />
-				<MyIcon name="moon" size={IconSize.Large} />
-				<MyIcon name="credit card" size={IconSize.Large} />
-				<MyIcon name="info" size={IconSize.Large} />
-				<MyIcon name="check" size={IconSize.Large} />
-				<MyIcon name="heart" size={IconSize.Large} />
-			</div> */}
-			<div>
-				<MyIcon name="home" />
-				<MyIcon name="users" size={IconSize.Large} color="blue" />
-				<MyIcon name="search" size={IconSize.Large} color="purple" />
-				<MyIcon name="star" size={IconSize.Large} color="green" />
-				<MyIcon name="moon" size={IconSize.Large} color="grey" />
-				<MyIcon name="credit card" size={IconSize.Large} color="teal" />
-				<MyIcon name="info" size={IconSize.Large} color="pink" />
-				<MyIcon name="check" size={IconSize.Large} color="brown" />
-				<MyIcon name="heart" size={IconSize.Large} color="olive" />
-			</div>
 		</div>
 	);
 }
