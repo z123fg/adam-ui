8260e33a5ce3a28b634ccab3e0ea30625320147c
Steve Pae <stevepaemusic@gmail.com>
2021-10-01 22:27:15 -0400

    - add dummy data

---
+++ src/data/imageData.js
@@ -0,0 +1,25 @@
+const imageData = [
+  {
+    id: Math.random() * 5,
+    src: "https://source.unsplash.com/collection/190727/1600x900",
+  },
+
+  {
+    id: Math.random() * 5,
+    src: "https://source.unsplash.com/collection/190728/1600x900",
+  },
+  {
+    id: Math.random() * 5,
+    src: "https://source.unsplash.com/collection/190726/1600x900",
+  },
+  {
+    id: Math.random() * 5,
+    src: "https://source.unsplash.com/collection/190725/1600x900",
+  },
+  {
+    id: Math.random() * 5,
+    src: "https://source.unsplash.com/collection/190723/1600x900",
+  },
+];
+
+export default imageData;
