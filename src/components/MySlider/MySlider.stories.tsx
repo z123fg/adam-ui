73e524881e0b1738f061d91c93b78f6d5f2ad8ef
Steve Pae <stevepaemusic@gmail.com>
2021-10-03 23:27:13 -0400

    update story

--- src/components/MySlider/MySlider.stories.tsx
+++ src/components/MySlider/MySlider.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import React from "react";
-import MySlider, { ISliderProps, SliderSize } from "./MySlider";
+import MySlider, { SliderSize } from "./MySlider";
 import imageData from "../../data/imageData";
 
 export default {
@@ -8,17 +7,20 @@ export default {
   component: MySlider,
 } as ComponentMeta<typeof MySlider>;
 
-// const Template: ComponentStory<ISliderProps> = (args) => <MySlider {...args}></MySlider></MySlider>;
-
-export const Default = (args) => (
-  const { children, sliderSize, photos, imageData } = args;
-  <MySlider {...args}>
-    <div className="slider-container">
-      <div className="slider-inner" style={{ width: photos * 100 + "%" }}>
-        {imageData.map((image) => (
-          <img key={image.id} src={image.src} />
-        ))}
-      </div>
-    </div>
-  </MySlider>
+const Template: ComponentStory<typeof MySlider> = (args) => (
+  <MySlider {...args} imageData={imageData} photos={5} />
 );
+
+export const Default = Template.bind({});
+Default.args = {
+  sliderSize: SliderSize.Small,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  sliderSize: SliderSize.Large,
+};
+
+// export const Default = () => (
+//   <MySlider imageData={imageData} photos={5} sliderSize={SliderSize.Small} />
+// );
